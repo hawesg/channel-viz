f7f93a13c004086d3f1eb9fb5694ce0a5dc92fb7
Garrett Hawes <ghawes@uci.edu>
2014-06-23 01:57:38 -0700

    add sensor4

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 xively.setKey('bKmY9Tj3153tYjWcTrEMmR6ZlPDLxzufq1RWiqxFxTsEv2Q6');
+angular.module('myModule', ['ui.bootstrap']);
 function MainCtrl($scope) {
   $scope.datastreams = {
     RH1: 65,
@@ -7,20 +8,11 @@ function MainCtrl($scope) {
 	Temp2: 65,
 	RH3: 65,
 	Temp3: 65,
+	RH4: 65,
+	Temp4: 65,
   };
-  //var datastreams = {
-  //  user_present: 'false',
-  //  total_coins: 0,
-  //};
-  //$scope.datastreams = {
-  //  get user_present( ) { return datastreams.user_present; },
-  //  set user_present(x) { datastreams.user_present = x; },
-  //  get total_coins( ) { return datastreams.total_coins; },
-  //  set total_coins(x) { datastreams.total_coins = x; },
-  //};
   $scope.feed_id = "657709964";
   angular.forEach(Object.keys($scope.datastreams), function( ds ) {
-  //for ds in Object.keys($scope.datastreams) {
     xively.datastream.get( $scope.feed_id, ds, function( data ) {  
       $scope.$apply(function(){
         $scope.datastreams[ds] = data.current_value;
