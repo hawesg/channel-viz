a6a6e22c9b627df2736071c2a3d2e841bfca4231
Garrett Hawes <ghawes@uci.edu>
2014-06-24 01:21:22 -0700

    add min and max and some formatting

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 xively.setKey('bKmY9Tj3153tYjWcTrEMmR6ZlPDLxzufq1RWiqxFxTsEv2Q6');
 angular.module('myModule', ['ui.bootstrap']);
 function MainCtrl($scope) {
+  $scope.max = {
+	RH1: 65,
+	RH2: 65,
+	RH3: 65,
+	RH4: 65,
+	Temp1: 65,
+	Temp2: 65,
+	Temp3: 65,
+	Temp4: 65,
+  };
+  $scope.min = {
+	RH1: 65,
+	RH2: 65,
+	RH3: 65,
+	RH4: 65,
+	Temp1: 65,
+	Temp2: 65,
+	Temp3: 65,
+	Temp4: 65,
+  };
   $scope.datastreams = {
     RH1: 65,
     Temp1: 65,
@@ -16,10 +36,14 @@ function MainCtrl($scope) {
     xively.datastream.get( $scope.feed_id, ds, function( data ) {  
       $scope.$apply(function(){
         $scope.datastreams[ds] = data.current_value;
+		$scope.max[ds] = data.max_value;
+		$scope.min[ds] = data.min_value;
       });
       xively.datastream.subscribe( $scope.feed_id, ds, function( event, data ) {
         $scope.$apply(function(){
           $scope.datastreams[ds] = data.current_value;
+		  $scope.max[ds] = data.max_value;
+		  $scope.min[ds] = data.min_value;
         });
       });
     });
